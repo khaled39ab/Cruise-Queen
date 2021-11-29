46c70bcb1591baa6da91dc9070e529e6127af8c4
khaled39ab <khaled39ab@gmail.com>
2021-11-29 11:44:56 +0600

    handle all function in one function

--- main.js
+++ main.js
@@ -1,33 +1,49 @@
+function handleTicket(ticket, isIncrease){
+    const ticketInput = document.getElementById (ticket +'Count');
+    const ticketCount = parseInt (ticketInput.value);
+    let ticketNewCount = ticketCount;
+    if (isIncrease == true){
+        ticketNewCount = ticketCount + 1 ;
+    }
+    if (isIncrease == false && ticketCount > 0){
+        ticketNewCount = ticketCount - 1 ;
+    }
+    ticketInput.value = ticketNewCount ;
+}
 document.getElementById ('firstClassIncrease').addEventListener ('click', function(){
-    const firstClassInput = document.getElementById ('firstClassCount');
-    const firstClassCount = parseInt (firstClassInput.value);
-    const firstClassNewCount = firstClassCount + 1 ;
-    firstClassInput.value = firstClassNewCount ;
+    handleTicket('firstClass', true);
+    // const firstClassInput = document.getElementById ('firstClassCount');
+    // const firstClassCount = parseInt (firstClassInput.value);
+    // const firstClassNewCount = firstClassCount + 1 ;
+    // firstClassInput.value = firstClassNewCount ;
 });
 
 document.getElementById ('firstClassDecrease').addEventListener ('click', function(){
-    const firstClassInput = document.getElementById ('firstClassCount');
-    const firstClassCount = parseInt (firstClassInput.value);
-    let firstClassNewCount = firstClassCount;
-    if (firstClassCount > 0){
-        firstClassNewCount = firstClassCount - 1 ;
-    }
-    firstClassInput.value = firstClassNewCount ;
+    handleTicket('firstClass', false);
+    // const firstClassInput = document.getElementById ('firstClassCount');
+    // const firstClassCount = parseInt (firstClassInput.value);
+    // let firstClassNewCount = firstClassCount;
+    // if (firstClassCount > 0){
+    //     firstClassNewCount = firstClassCount - 1 ;
+    // }
+    // firstClassInput.value = firstClassNewCount ;
 });
 
 document.getElementById ('economyIncrease').addEventListener ('click', function(){
-    const economyInput = document.getElementById ('economyCount');
-    const economyCount = parseInt (economyInput.value);
-    const economyNewCount = economyCount + 1 ;
-    economyInput.value = economyNewCount;
+    handleTicket('economy', true);
+    // const economyInput = document.getElementById ('economyCount');
+    // const economyCount = parseInt (economyInput.value);
+    // const economyNewCount = economyCount + 1 ;
+    // economyInput.value = economyNewCount;
 });
 
 document.getElementById ('economyDecrease').addEventListener ('click', function(){
-    const economyInput = document.getElementById ('economyCount');
-    const economyCount = parseInt (economyInput.value);
-    let economyNewCount = economyCount ;
-    if (economyCount > 0){
-        economyNewCount = economyCount - 1 ;
-    }
-    economyInput.value = economyNewCount;
+    handleTicket('economy', false);
+    // const economyInput = document.getElementById ('economyCount');
+    // const economyCount = parseInt (economyInput.value);
+    // let economyNewCount = economyCount ;
+    // if (economyCount > 0){
+    //     economyNewCount = economyCount - 1 ;
+    // }
+    // economyInput.value = economyNewCount;
 })
